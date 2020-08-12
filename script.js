b5ee2718eaeadd3a0488efbf6d73dd9cd88ff26b
Nataliia <n3v3st4@gmail.com>
2020-08-12 21:14:47 +0300

    задания на promise - done

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 
 // Для генерации случайного числа(индекс элемента массива цветов), используй функцию randomIntegerFromInterval.
 
-const randomIntegerFromInterval = (min, max) => {
+const randomIntegerFromInterv = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
@@ -43,7 +43,7 @@ function changeStartBtnStatus() {
 function onStartBtnClick() {
   changeStartBtnStatus();
   intervalId = setInterval(() => {
-    bodyColorSet(colors[randomIntegerFromInterval(0, colors.length - 1)]);
+    bodyColorSet(colors[randomIntegerFromInterv(0, colors.length - 1)]);
   }, 1000);
 }
 
@@ -92,3 +92,92 @@ function updateTimerValue(time) {
 function pad(value) {
   return String(value).padStart(2, "0");
 }
+
+//
+//
+// -------------  ПРОМИСЫ ------------
+//
+// ----------- № 1 ------------------------------------
+// Напиши функцию delay(ms), которая возвращает промис, переходящий в состояние "resolved" через ms миллисекунд.Значением исполнившегося промиса должно быть то кол - во миллисекунд которое передали во время вызова функции delay.
+
+const delay = (ms) => {
+  return new Promise((res, rej) => {
+    setTimeout(() => res(ms));
+  }, ms);
+};
+
+const logg = (time) => console.log(`Resolved after ${time}ms`);
+
+// Вызовы функции для проверки
+delay(2000).then(logg); // Resolved after 2000ms
+delay(1000).then(logg); // Resolved after 1000ms
+delay(1500).then(logg); // Resolved after 1500ms
+
+//
+// -------------- # 2 ------------------------------------------------------
+// Перепиши функцию toggleUserState() так, чтобы она не использовала callback - функцию callback, а принимала всего два параметра allUsers и userName и возвращала промис.
+
+const users = [
+  { name: "Mango", active: true },
+  { name: "Poly", active: false },
+  { name: "Ajax", active: true },
+  { name: "Lux", active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+  return new Promise((res) => {
+    const updatedUsers = allUsers.map((user) =>
+      user.name === userName ? { ...user, active: !user.active } : user
+    );
+    res(updatedUsers);
+  });
+};
+
+const logger = (updatedUsers) => console.table(updatedUsers);
+/*
+ * Должно работать так
+ */
+toggleUserState(users, "Mango").then(logger);
+toggleUserState(users, "Lux").then(logger);
+
+//
+// ----------------- # 3 ------------------------------------------------------------
+// Перепиши функцию makeTransaction() так, чтобы она не использовала callback - функции onSuccess и onError, а принимала всего один параметр transaction и возвращала промис.
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const makeTransaction = (transaction) => {
+  const delay = randomIntegerFromInterval(200, 500);
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const canProcess = Math.random() > 0.3;
+
+      if (canProcess) {
+        resolve([transaction.id, delay]);
+      }
+      reject(transaction.id);
+    }, delay);
+  });
+  return promise;
+};
+
+const logSuccess = ([id, time]) => {
+  console.log(`Transaction ${id} processed in ${time}ms`);
+};
+
+const logError = (id) => {
+  console.warn(`Error processing transaction ${id}. Please try again later.`);
+};
+
+/*
+ * Должно работать так
+ */
+makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
+
+makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
+
+makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
+
+makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
